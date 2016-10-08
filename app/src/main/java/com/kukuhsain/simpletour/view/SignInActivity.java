dd8a4cc0d48b0e75b9eeca99a991f19df7b1a537
Kukuh <kukuhsain@gmail.com>
2016-10-08 12:35:05 +0700

    Add Timber dependency and Retrolambda support to the project

--- app/src/main/java/com/kukuhsain/simpletour/view/SignInActivity.java
+++ app/src/main/java/com/kukuhsain/simpletour/view/SignInActivity.java
@@ -1,7 +1,10 @@
 package com.kukuhsain.simpletour.view;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.kukuhsain.simpletour.R;
 
@@ -10,9 +13,23 @@ import com.kukuhsain.simpletour.R;
  */
 
 public class SignInActivity extends AppCompatActivity {
+
+    TextInputEditText etEmail = (TextInputEditText) findViewById(R.id.et_email);
+    TextInputEditText etPassword = (TextInputEditText) findViewById(R.id.et_password);
+    Button btnSignIn = (Button) findViewById(R.id.btn_sign_in);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    private void setupSignInButton() {
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
