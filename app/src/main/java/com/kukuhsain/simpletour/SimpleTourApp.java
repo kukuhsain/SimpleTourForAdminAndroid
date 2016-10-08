6f5d6b64651f606fc9dcdcc2d8f14f31071a8db0
Kukuh <kukuhsain@gmail.com>
2016-10-08 10:02:50 +0700

    Create a simple Hello World which implementing Splash screen

---
+++ app/src/main/java/com/kukuhsain/simpletour/SimpleTourApp.java
@@ -0,0 +1,14 @@
+package com.kukuhsain.simpletour;
+
+import android.app.Application;
+
+/**
+ * Created by kukuh on 08/10/16.
+ */
+
+public class SimpleTourApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/kukuhsain/simpletour/view/PackagesActivity.java
@@ -0,0 +1,19 @@
+package com.kukuhsain.simpletour.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kukuhsain.simpletour.R;
+
+/**
+ * Created by kukuh on 08/10/16.
+ */
+
+public class PackagesActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_packages);
+    }
+}
