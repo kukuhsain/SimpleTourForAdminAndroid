97f82ceca55ee330f430ddc39edee7963e2accff
Kukuh <kukuhsain@gmail.com>
2016-10-12 12:30:49 +0700

    Add back button to Packages page

--- app/src/main/java/com/kukuhsain/simpletour/view/PackagesActivity.java
+++ app/src/main/java/com/kukuhsain/simpletour/view/PackagesActivity.java
@@ -1,18 +1,30 @@
 package com.kukuhsain.simpletour.view;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.kukuhsain.simpletour.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by kukuh on 08/10/16.
  */
 
 public class PackagesActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.collapsing_toolbar) CollapsingToolbarLayout collapsingToolbarLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_packages);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
