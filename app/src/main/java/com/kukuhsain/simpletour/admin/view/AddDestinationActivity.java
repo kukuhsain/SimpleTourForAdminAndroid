e24f2818d763b3d7af4d3ceeb23d9f4aca38ff75
Kukuh <kukuhsain@gmail.com>
2016-10-22 11:04:43 +0700

    Add image picker in add destination form

--- app/src/main/java/com/kukuhsain/simpletour/admin/view/AddDestinationActivity.java
+++ app/src/main/java/com/kukuhsain/simpletour/admin/view/AddDestinationActivity.java
@@ -1,24 +1,35 @@
 package com.kukuhsain.simpletour.admin.view;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.kukuhsain.simpletour.admin.R;
 import com.kukuhsain.simpletour.admin.model.remote.SimpleTourApi;
 
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import rx.schedulers.Schedulers;
+import timber.log.Timber;
 
 /**
  * Created by kukuh on 20/10/16.
  */
 
 public class AddDestinationActivity extends AppCompatActivity {
+    private final int PICK_IMAGE_REQUEST = 1;
+
+    @BindView(R.id.iv_preview_image) ImageView ivPreviewImage;
     @BindView(R.id.et_title) EditText etTitle;
     @BindView(R.id.et_content) EditText etContent;
     @BindView(R.id.et_location) EditText etLocation;
@@ -50,5 +61,34 @@ public class AddDestinationActivity extends AppCompatActivity {
                         Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
                     });
                 });
+    }
+
+    @OnClick(R.id.btn_insert_image)
+    public void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Timber.d("onActivityResult...");
+        Timber.d("Pick Image:... "+PICK_IMAGE_REQUEST);
+        Timber.d("requestCode:... "+requestCode);
+        Timber.d("resultCode:... "+resultCode);
+        Timber.d("Result OK:... "+RESULT_OK);
+        if (requestCode==PICK_IMAGE_REQUEST && data!=null && data.getData()!=null) {
+            Uri uri = data.getData();
+            Timber.d("image uri:... "+uri);
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                ivPreviewImage.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
