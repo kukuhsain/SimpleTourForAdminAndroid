d4cf5da5a635b9ca9bf5116f62c9633954116eb2
Kukuh <kukuhsain@gmail.com>
2016-11-04 10:27:27 +0700

    Implement Sign Up functionality

--- app/src/main/java/com/kukuhsain/simpletour/host/view/SignUpActivity.java
+++ app/src/main/java/com/kukuhsain/simpletour/host/view/SignUpActivity.java
@@ -1,15 +1,21 @@
 package com.kukuhsain.simpletour.host.view;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.widget.Toast;
 
 import com.kukuhsain.simpletour.host.R;
+import com.kukuhsain.simpletour.host.model.local.PreferencesHelper;
+import com.kukuhsain.simpletour.host.model.remote.SimpleTourApi;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import timber.log.Timber;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by kukuh on 08/10/16.
@@ -21,6 +27,8 @@ public class SignUpActivity extends AppCompatActivity {
     @BindView(R.id.et_password) TextInputEditText etPassword;
     @BindView(R.id.et_phone) TextInputEditText etPhone;
 
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +38,59 @@ public class SignUpActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_sign_up)
     public void signUp() {
-        Timber.d("Sign Up...");
-        Timber.d(etName.getText().toString());
-        Timber.d(etEmail.getText().toString());
-        Timber.d(etPassword.getText().toString());
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String phone = etPhone.getText().toString();
+
+        if (name.isEmpty()) {
+            etName.setError("Please insert your name!");
+            etName.requestFocus();
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Please insert a valid email!");
+            etEmail.requestFocus();
+        } else if (password.isEmpty()) {
+            etPassword.setError("Please insert password!");
+            etPassword.requestFocus();
+        } else if (!Patterns.PHONE.matcher(phone).matches()) {
+            etPhone.setError("Please insert a valid phone number!");
+            etPhone.requestFocus();
+        } else {
+            showLoading();
+            SimpleTourApi.getInstance()
+                    .register(name, email, password, phone)
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(accessToken -> {
+                        PreferencesHelper.getInstance().putAccessToken(accessToken);
+                        /*PreferencesHelper.getInstance().putLoggedInStatus(true);*/
+                        runOnUiThread(() -> {
+                            dismissLoading();
+                            startActivity(new Intent(this, DestinationsActivity.class));
+                            finish();
+                        });
+                    }, throwable -> {
+                        runOnUiThread(() -> {
+                            dismissLoading();
+                            Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_SHORT).show();
+                        });
+                    });
+
+        }
+    }
+
+    private void showLoading() {
+        if (progressDialog != null) {
+            progressDialog.show();
+        } else {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Please wait...");
+            progressDialog.show();
+        }
+    }
+
+    private void dismissLoading() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+        }
     }
 }
