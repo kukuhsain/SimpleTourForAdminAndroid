706c9c527540c139b81b09f1b6f5db9bb88ba107
Kukuh <kukuhsain@gmail.com>
2016-11-07 14:01:58 +0700

    Show simple notification after user receiving a message from FCM

--- app/src/main/java/com/kukuhsain/simpletour/host/model/remote/FireService.java
+++ app/src/main/java/com/kukuhsain/simpletour/host/model/remote/FireService.java
@@ -1,7 +1,13 @@
 package com.kukuhsain.simpletour.host.model.remote;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v7.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.kukuhsain.simpletour.host.R;
 
 import timber.log.Timber;
 
@@ -21,5 +27,13 @@ public class FireService extends FirebaseMessagingService {
         Timber.d(remoteMessage.getNotification().getTitle());
         Timber.d(remoteMessage.getNotification().getBody());
         Timber.d(remoteMessage.getNotification().getIcon());
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
