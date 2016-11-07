4ea5c80cea7b2402df5fa1148ac031e7c6ab0f7b
Kukuh <kukuhsain@gmail.com>
2016-11-07 16:08:46 +0700

    Show the package list to PackagesActivity by implementing getPackages API

--- app/src/main/java/com/kukuhsain/simpletour/host/view/PackagesActivity.java
+++ app/src/main/java/com/kukuhsain/simpletour/host/view/PackagesActivity.java
@@ -3,9 +3,12 @@ package com.kukuhsain.simpletour.host.view;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
@@ -13,9 +16,11 @@ import com.kukuhsain.simpletour.host.R;
 import com.kukuhsain.simpletour.host.model.pojo.Destination;
 import com.kukuhsain.simpletour.host.model.pojo.Package;
 import com.kukuhsain.simpletour.host.model.remote.SimpleTourApi;
+import com.kukuhsain.simpletour.host.view.adapter.PackageAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import rx.schedulers.Schedulers;
 import timber.log.Timber;
 
 /**
@@ -27,6 +32,10 @@ public class PackagesActivity extends AppCompatActivity {
     @BindView(R.id.collapsing_toolbar) CollapsingToolbarLayout collapsingToolbarLayout;
     @BindView(R.id.iv_background_image) ImageView ivBackgroundImage;
     @BindView(R.id.tv_description) TextView tvDescription;
+    @BindView(R.id.rv_packages) RecyclerView rvPackages;
+
+    private Destination destination;
+    private PackageAdapter packageAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,12 +47,34 @@ public class PackagesActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         String destinationString = getIntent().getStringExtra("destination");
-        Destination destination = (new Gson()).fromJson(destinationString, Destination.class);
+        destination = (new Gson()).fromJson(destinationString, Destination.class);
+
         collapsingToolbarLayout.setTitle(destination.getTitle());
         tvDescription.setText(destination.getContent());
         Glide.with(this)
                 .load(SimpleTourApi.BASE_URL+destination.getImageUrl())
                 .into(ivBackgroundImage);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rvPackages.setLayoutManager(layoutManager);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SimpleTourApi.getInstance().getPackages(destination.getDestinationId())
+                .subscribeOn(Schedulers.io())
+                .subscribe(packages -> {
+                    packageAdapter = new PackageAdapter(this, packages);
+                    runOnUiThread(() -> rvPackages.setAdapter(packageAdapter));
+                }, throwable -> {
+                    throwable.printStackTrace();
+                    runOnUiThread(() -> Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_SHORT).show());
+                });
     }
 
     public void onItemClicked(Package onePackage) {
