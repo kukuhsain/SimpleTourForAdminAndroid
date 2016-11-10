29733bfa93e9edce2abb367a3d6c0de6ba64963e
Kukuh <kukuhsain@gmail.com>
2016-11-10 12:15:49 +0700

    Add PackageDetailActivity and it's layout

---
+++ app/src/main/java/com/kukuhsain/simpletour/host/view/PackageDetailActivity.java
@@ -0,0 +1,17 @@
+package com.kukuhsain.simpletour.host.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kukuh on 10/11/16.
+ */
+
+public class PackageDetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
